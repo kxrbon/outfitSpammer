5d08b620403e3a4dbfd19b294e48054693d59178
kxrbon <94630390+kxrbon@users.noreply.github.com>
2022-11-19 10:00:21 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ while counter < 50 do
 	local Event = game:GetService("ReplicatedStorage"):WaitForChild("⠀⠀⠀⠀😃_outfit_⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
 	Event:FireServer(arg)
 	counter = counter + 1
-	wait(0.1)
+	wait(1)
 end
 
 game:GetService("TeleportService"):Teleport(4984400432, game.Players.LocalPlayer)
